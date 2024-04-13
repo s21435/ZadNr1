ac0ace6e729af725a47999e05e384d217e2870b4
s21435 <127010711+s21435@users.noreply.github.com>
2024-04-13 12:16:16 +0200

    getAvarage wykonanie
    getAvarage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,3 +3,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World 1!");
 Console.WriteLine("Hello, World 2!");
 Console.WriteLine("Hello, World 3!");
+
+static double getAvarage(int[] tab)
+{
+    double avarage = 0, sum = 0;
+    for (int i = 0; i < tab.Length - 1; i++)
+    {
+        sum += tab[i];
+
+    }
+    avarage = sum / tab.Length;
+    return avarage;
+}
+int[] tab = [1,2,3,4,5,6,7,8,9,9];
+Console.WriteLine(getAvarage(tab));
